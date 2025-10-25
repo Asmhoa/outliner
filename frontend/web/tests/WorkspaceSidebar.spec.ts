7b924d60c6052fdb84d946e5eb74cc5754a5978b
Amol Sharma <amol_s@outlook.com>
2025-10-24 17:46:06 -0700

    Workspace - add, edit, delete tests

--- frontend/web/tests/WorkspaceSidebar.spec.ts
+++ frontend/web/tests/WorkspaceSidebar.spec.ts
@@ -24,4 +24,44 @@ test.describe('Workspace Sidebar', () => {
     const workspaceTabs = page.getByRole('tab');
     await expect(workspaceTabs.first()).toBeVisible();
   });
+
+  test('should add, edit, and delete a workspace', async ({
+    page,
+  }) => {
+    await page.setViewportSize({ width: 1920, height: 1080 });
+    await page.goto('/');
+
+    await page.getByTestId('add-workspace-button').click();
+
+    await expect(page.getByText('Create New Workspace')).toBeVisible();
+
+    const workspaceName = `WS${Date.now().toString().slice(-5)}`;
+    await page.getByLabel('Workspace Name').fill(workspaceName);
+    await page.getByRole('button', { name: 'Create Workspace' }).click();
+
+    await expect(page.getByRole('tab', { name: workspaceName })).toBeVisible();
+
+    // Edit the workspace
+    await page.getByRole('tab', { name: workspaceName }).dblclick();
+    await expect(page.getByText('Edit Workspace')).toBeVisible();
+
+    const updatedWorkspaceName = `WS${Date.now().toString().slice(-5)}`;
+    await page.getByLabel('Workspace Name').fill(updatedWorkspaceName);
+    await page.getByLabel('Workspace Color').fill('#fa5252');
+    await page.getByLabel('Workspace Name').click(); // Close the color picker
+    await page.getByRole('button', { name: 'Update Workspace' }).click();
+
+    await expect(
+      page.getByRole('tab', { name: updatedWorkspaceName }),
+    ).toBeVisible();
+
+    // Delete the workspace
+    await page.getByRole('tab', { name: updatedWorkspaceName }).dblclick();
+    await expect(page.getByText('Edit Workspace')).toBeVisible();
+    await page.getByRole('button', { name: 'Delete Workspace' }).click();
+    await page.getByRole('button', { name: 'Confirm Delete' }).click();
+    await expect(
+      page.getByRole('tab', { name: updatedWorkspaceName }),
+    ).not.toBeVisible();
+  });
 });
