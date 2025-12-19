830f9bc8c50aefca5cab644f249cea50402927bf
Amol Sharma <amol_s@outlook.com>
2025-12-18 18:49:42 -0800

    Refactor errors

--- packages/backend/src/database/errors.ts
+++ packages/backend/src/database/errors.ts
@@ -2,44 +2,45 @@
  * Custom exception classes for the Outliner API server.
  */
 
-export class PageAlreadyExistsError extends Error {
+abstract class OutlinerDBError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = this.constructor.name;
+  }
+}
+
+export class PageAlreadyExistsError extends OutlinerDBError {
   constructor(message: string = "Page already exists") {
     super(message);
-    this.name = "PageAlreadyExistsError";
   }
 }
 
-export class PageNotFoundError extends Error {
+export class PageNotFoundError extends OutlinerDBError {
   constructor(message: string = "Page not found") {
     super(message);
-    this.name = "PageNotFoundError";
   }
 }
 
-export class WorkspaceNotFoundError extends Error {
+export class WorkspaceNotFoundError extends OutlinerDBError {
   constructor(message: string = "Workspace not found") {
     super(message);
-    this.name = "WorkspaceNotFoundError";
   }
 }
 
-export class BlockNotFoundError extends Error {
+export class BlockNotFoundError extends OutlinerDBError {
   constructor(message: string = "Block not found") {
     super(message);
-    this.name = "BlockNotFoundError";
   }
 }
 
-export class UserDatabaseNotFoundError extends Error {
+export class UserDatabaseNotFoundError extends OutlinerDBError {
   constructor(message: string = "User database not found") {
     super(message);
-    this.name = "UserDatabaseNotFoundError";
   }
 }
 
-export class UserDatabaseAlreadyExistsError extends Error {
+export class UserDatabaseAlreadyExistsError extends OutlinerDBError {
   constructor(message: string = "User database already exists") {
     super(message);
-    this.name = "UserDatabaseAlreadyExistsError";
   }
 }
